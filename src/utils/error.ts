44c4f52ac41ef6b7e7d269d0b108f248cbf58f9a
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-12-26 16:45:27 +0500

    updete

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -3,18 +3,21 @@ export default function error(e: any) {
     const status = e?.status;
     const detail = e?.response?.data?.detail;
     switch (status) {
-        case 400:
+        case 400: { }
             toast('error', detail)
-
+            break
         case 401:
             // throw new Error('Unauthorized');
+            break
         case 403:
             // throw new Error('Forbidden');
+            break
         case 404:
             // throw new Error('Not Found');
+            break
         case 500:
             toast('error', 'Internet bilan aloqa yo\'q')
-
+            break
         default:
     }
 
